df9a800652906282d2ccc49e711046fe954b391f
Alberto Nicoletti <albyx.n@gmail.com>
2021-11-28 19:59:47 +0100

    chore: update vercel stuff

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,6 @@
-// @generated: @expo/next-adapter@3.1.7
-// Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#shared-steps
-
-module.exports = { presets: ["@expo/next-adapter/babel"] };
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+  };
+};
